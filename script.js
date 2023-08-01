3c23221b2e00975ae5efd7b2c7dced0ef28ac33d
Dunni <pelumioladunni3@gmail.com>
2023-08-01 12:37:56 +0100

    deleted some files & made some changes

--- script.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById("btnLP")
-button.addEventListener("click", function(){
-    console.log("go")
-     document.location.href="index2.html";
-});
-console.log(document.getElementById("btn"))
-
-
